6535f3bfa6b9f402f47fa121a02b1209fab5758a
Elena <zorenkoelen@gmail.com>
2022-01-16 21:30:36 +0200

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-  const name = readlineSync.question('May I have your name? ');
+  const text = readlineSync.question('May I have your name? ');
+  const name = (text === '') ? 'new player' : text;
   console.log(`Hello, ${name}!`);
 };
