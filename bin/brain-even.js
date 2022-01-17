178f930b48d211a63ab381dbd4ab52f192d9c491
Elena <zorenkoelen@gmail.com>
2022-01-17 20:50:11 +0200

    Update brain-even.js, brain-games.js, cli.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,9 +3,10 @@ import readlineSync from 'readline-sync';
 
 const getRandomInt = () => Math.floor(Math.random() * 100);
 const evenRandomInt = (number) => {
-  if (number % 2 == 0) return 'yes';
-  return 'no'
+  if (number % 2 === 0) return 'yes';
+  return 'no';
 };
+const numberOfRounds = 3;
 
 console.log('Welcome to the Brain Games!');
 const name = () => {
@@ -14,29 +15,28 @@ const name = () => {
 };
 const correctName = name();
 console.log(`Hello, ${correctName}!`);
-console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 const checkNumber = () => {
   let flag = true;
   let i = 1;
-  while (i < 4 && flag) {
-
-    let newNumber = getRandomInt();
-    let even = evenRandomInt(newNumber);
+  while (i < (numberOfRounds + 1) && flag) {
+    const newNumber = getRandomInt();
+    const even = evenRandomInt(newNumber);
 
     console.log(`Question: ${newNumber}`);
-    let answer = readlineSync.question('Your answer: ');
+    const answer = readlineSync.question('Your answer: ');
 
     if (answer === even) {
-      console.log(`Correct!`);
+      console.log('Correct!');
       i += 1;
     } else {
       console.log(`"${answer}"  is wrong answer ;(. Correct answer was "${even}". Let's try again, ${correctName}!`);
       flag = false;
     }
   }
-  if (i === 4) {
+  if (i === (numberOfRounds + 1)) {
     console.log(`Congratulations, ${correctName}!`);
-  };
+  }
 };
 checkNumber();
