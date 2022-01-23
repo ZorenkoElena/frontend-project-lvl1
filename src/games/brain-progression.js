d8ac07cf9d91b171fe1bef72a17052e692a676a9
Elena <zorenkoelen@gmail.com>
2022-01-23 23:10:58 +0200

    Add bin/brain-progression.js Add src/games/brain-progression.js

---
+++ src/games/brain-progression.js
@@ -0,0 +1,25 @@
+import mainPartOfGame from '../index.js';
+
+const getRandomInt = (min, max) => {
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+const mainQuestion = 'What number is missing in the progression?';
+const lengthOfProgression = getRandomInt(6, 12);
+
+const stepOfgame = () => {
+  const startOfProgression = getRandomInt(1, 20);
+  const differenceOfProgression = getRandomInt(2, 5);
+  const indexOfAsking = getRandomInt(0, (lengthOfProgression - 1));
+  const newProgression = [];
+  let a = startOfProgression;
+  for (let i = 0; i < lengthOfProgression; i += 1) {
+    newProgression.push(a);
+    a += differenceOfProgression;
+  }
+  const rightAnswer = String(newProgression[indexOfAsking]);
+  newProgression[indexOfAsking] = '..';
+  const question = newProgression.join(' ');
+  return [rightAnswer, question];
+};
+export default () => mainPartOfGame(mainQuestion, stepOfgame);
